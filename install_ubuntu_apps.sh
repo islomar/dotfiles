640ca683af1105c71343aade54bb0edf2d342d64
Isidro López <islomar@gmail.com>
2020-02-20 18:11:33 +0100

    Update install_ubuntu_apps.sh

--- install_ubuntu_apps.sh
+++ install_ubuntu_apps.sh
@@ -67,7 +67,7 @@ sudo apt install apt-transport-https ca-certificates curl software-properties-co
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
 sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu bionic stable"
 sudo apt update
-sudo apt install docker-ce -y
+sudo apt install docker-ce docker-compose -y
 sudo usermod -aG docker islomar
 
 # SDKMan
