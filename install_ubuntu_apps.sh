7b121c906a1f5741201e4737c5d8799e121c3509
Isidro López <islomar@gmail.com>
2020-02-21 14:51:33 +0100

    Update install_ubuntu_apps.sh

--- install_ubuntu_apps.sh
+++ install_ubuntu_apps.sh
@@ -5,6 +5,7 @@ set -e
 # TO DO
 # dpkg -l <packageName>
 
+cd ~
 sudo apt-get update -y
 sudo apt-get upgrade -y
 sudo apt install vim tmux tmate curl git npm yarn maven gitk git-gui silversearcher-ag htop ctop virtualbox vagrant xclip gnome-shell-pomodoro -y
@@ -30,6 +31,19 @@ sudo apt-get install fonts-powerline -y
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git "$HOME/.zsh-syntax-highlighting" --depth 1
 echo "source $HOME/.zsh-syntax-highlighting/zsh-syntax-highlighting.zsh" >> "$HOME/.zshrc"
+
+# Install Golang: https://www.digitalocean.com/community/tutorials/how-to-install-go-on-ubuntu-18-04
+## Check the last release: https://golang.org/dl/
+cd ~
+curl -O https://dl.google.com/go/go1.13.8.linux-amd64.tar.gz
+tar xvf go1.10.3.linux-amd64.tar.gz
+sudo chown -R root:root ./go
+sudo mv go /usr/local
+rm https://dl.google.com/go/go1.13.8.linux-amd64.tar.gz
+cd -
+# Set Go Paths manually (PATH and GOPATH) in ~/.profile
+echo "export GOPATH=$HOME/go" >> ~/.zshrc
+echo "export PATH=$PATH:/usr/local/go/bin:$GOPATH/bin" >> ~/.zshrc
 
 # Python
 sudo apt-get install libmysqlclient-dev libpq-dev python-dev -y
