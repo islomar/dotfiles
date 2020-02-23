a8d1d8f1db20a2098b8b5d263b146595399c11ed
Isidro López <islomar@gmail.com>
2020-02-23 19:17:35 +0100

    Update install_ubuntu_apps.sh

--- install_ubuntu_apps.sh
+++ install_ubuntu_apps.sh
@@ -38,6 +38,7 @@ curl -sS https://getcomposer.org/installer -o composer-setup.php
 HASH="$(wget -q -O - https://composer.github.io/installer.sig)"
 php -r "if (hash_file('SHA384', 'composer-setup.php') === '$HASH') { echo 'Installer verified'; } else { echo 'Installer corrupt'; unlink('composer-setup.php'); } echo PHP_EOL;"
 sudo php composer-setup.php --install-dir=/usr/local/bin --filename=composer
+rm composer-setup.php
 
 # Install Golang: https://www.digitalocean.com/community/tutorials/how-to-install-go-on-ubuntu-18-04
 ## Check the last release: https://golang.org/dl/
