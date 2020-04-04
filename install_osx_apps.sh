4d8a3dff94b18b99041c174775da7257f0f9b4dd
Isidro López <islomar@gmail.com>
2020-04-04 17:58:15 +0200

    Update install_osx_apps.sh

--- install_osx_apps.sh
+++ install_osx_apps.sh
@@ -36,6 +36,7 @@ cd fonts && ./install.sh && cd .. && rm -rf fonts
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
 git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
 
+curl -L https://iterm2.com/shell_integration/install_shell_integration.sh | bash
 
 brew cleanup
 
