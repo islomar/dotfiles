67ba4d36512b78afb7bf8a3b035c144359fe0aac
Isidro Lopez <islomar@gmail.com>
2020-04-26 19:15:04 +0200

    Add .gitattributes

--- scripts/install-home-files.sh
+++ scripts/install-home-files.sh
@@ -4,7 +4,9 @@ set -e
 
 # diff $HOME/.bashrc $HOME/.dotfiles/home/.bashrc
 
-#ln -s $HOME/.dotfiles/.bashrc $HOME/.bashrc
-#ln -s $HOME/.dotfiles/.zshrc $HOME/.zshrc
-#ln -s $HOME/.dotfiles/.zsenv $HOME/.zsenv
+ln -s $HOME/.dotfiles/.bashrc $HOME/.bashrc
+ln -s $HOME/.dotfiles/.zshrc $HOME/.zshrc
 ln -s $HOME/.dotfiles/.vimrc $HOME/.vimrc
+ln -s $HOME/.dotfiles/.gitignore_global $HOME/.gitignore_global
+ln -s $HOME/.dotfiles/.gitattributes $HOME/.gitattributes
+#ln -s $HOME/.dotfiles/.zsenv $HOME/.zsenv
