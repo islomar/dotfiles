4f60cc0c4c26779b12a2261036eb8a691370f498
Isidro Lopez <islomar@gmail.com>
2020-04-26 19:17:25 +0200

    Add .zsenv

--- scripts/install-home-files.sh
+++ scripts/install-home-files.sh
@@ -9,4 +9,5 @@ ln -s $HOME/.dotfiles/.zshrc $HOME/.zshrc
 ln -s $HOME/.dotfiles/.vimrc $HOME/.vimrc
 ln -s $HOME/.dotfiles/.gitignore_global $HOME/.gitignore_global
 ln -s $HOME/.dotfiles/.gitattributes $HOME/.gitattributes
-#ln -s $HOME/.dotfiles/.zsenv $HOME/.zsenv
+ln -s $HOME/.dotfiles/.zsenv $HOME/.zsenv
+#ln -s $HOME/.dotfiles/.gitconfig $HOME/.gitconfig
