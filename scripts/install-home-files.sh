d865930d63e5072100786fdca5f8645c0a625dc1
Isidro Lopez <islomar@gmail.com>
2020-04-26 18:57:36 +0200

    Reorganize the repo

---
+++ scripts/install-home-files.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+set -x
+set -e
+
+#ln -s $HOME/.dotfiles/.bashrc $HOME/.bashrc
+#ln -s $HOME/.dotfiles/.zshrc $HOME/.zshrc
+#ln -s $HOME/.dotfiles/.zsenv $HOME/.zsenv
+ln -s $HOME/.dotfiles/.vimrc $HOME/.vimrc
