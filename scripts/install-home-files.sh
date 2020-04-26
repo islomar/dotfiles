77525b3d988162adf6cbb474c5cf49f24b986a8c
Isidro Lopez <islomar@gmail.com>
2020-04-26 19:04:36 +0200

    Add .bashrc

--- scripts/install-home-files.sh
+++ scripts/install-home-files.sh
@@ -2,6 +2,8 @@
 set -x
 set -e
 
+# diff $HOME/.bashrc $HOME/.dotfiles/home/.bashrc
+
 #ln -s $HOME/.dotfiles/.bashrc $HOME/.bashrc
 #ln -s $HOME/.dotfiles/.zshrc $HOME/.zshrc
 #ln -s $HOME/.dotfiles/.zsenv $HOME/.zsenv
