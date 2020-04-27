269f8e3e7ef6b7d884e4b585ed72d784f5d26b9b
Isidro Lopez <islomar@gmail.com>
2020-04-27 17:39:00 +0200

    Sync files with OSX

--- scripts/install-home-files.sh
+++ scripts/install-home-files.sh
@@ -6,17 +6,17 @@ echo DOTFILES_FOLDER=$DOTFILES_FOLDER
 
 # diff $HOME/.bashrc $HOME/.dotfiles/home/.bashrc
 
-ln -sf $HOME/.dotfiles/.bashrc $HOME/.bashrc
-ln -sf $HOME/.dotfiles/.zshrc $HOME/.zshrc
-ln -sf $HOME/.dotfiles/.vimrc $HOME/.vimrc
-ln -sf $HOME/.dotfiles/.gitignore_global $HOME/.gitignore_global
-ln -sf $HOME/.dotfiles/.gitattributes $HOME/.gitattributes
-ln -sf $HOME/.dotfiles/.zsenv $HOME/.zsenv
-#ln -sf $HOME/.dotfiles/.gitconfig $HOME/.gitconfig
+ln -sf $HOME/.dotfiles/home/.bashrc $HOME/.bashrc
+ln -sf $HOME/.dotfiles/home/.zshrc $HOME/.zshrc
+ln -sf $HOME/.dotfiles/home/.vimrc $HOME/.vimrc
+ln -sf $HOME/.dotfiles/home/.gitignore_global $HOME/.gitignore_global
+ln -sf $HOME/.dotfiles/home/.gitattributes $HOME/.gitattributes
+ln -sf $HOME/.dotfiles/home/.zsenv $HOME/.zsenv
+#ln -sf $HOME/.dotfiles/home/.gitconfig $HOME/.gitconfig
 
 unamestr=`uname`
 if [[ "$unamestr" == 'Linux' ]]; then
     source $DOTFILES_FOLDER/linux/install-linux-files.sh
-elif [[ "$unamestr" == 'Mac OS X' ]]; then
+elif [[ "$unamestr" == 'Darwin' ]]; then
     source $DOTFILES_FOLDER/osx/install-osx-files.sh
 fi
